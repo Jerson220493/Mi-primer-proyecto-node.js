7368ef12fe2df2d295a419ece4f17d4b1e6d5533
unknown <jerson>
2023-11-13 23:56:17 -0500

    validar campos y duplicidad de usuarios

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -15,12 +15,13 @@ const formularioRegistro = (req, res)=>{
 
 const registrar = async (req, res)=>{ // aca vamos a registrar un usuario nuevo 
     // en este caso vamos a utilizar el req
-
+    
     //validacion
     await check('nombre').notEmpty().withMessage('El nombre no puede estar vacío').run(req);
     await check('email').isEmail().withMessage('Eso no parece un email').run(req);
     await check('password').isLength({ min:6 }).withMessage('El password debe ser de al menos 6 caracteres').run(req);
-    await check('repite_password').equals('password').withMessage('Los password no son iguales').run(req);
+    await check('repite_password').equals(req.body.password).withMessage('Los password no son iguales').run(req);
+
     let resultado = validationResult(req);
     // validattions Result almacena los check 
 
@@ -29,9 +30,31 @@ const registrar = async (req, res)=>{ // aca vamos a registrar un usuario nuevo
         // errores
         return res.render('auth/registro', {
             pagina : 'Crear Cuenta',
-            errores : resultado.array()
+            errores : resultado.array(),
+            usuario :{ 
+                nombre : req.body.nombre,
+                email : req.body.email
+            }
         })
     }
+
+    // extraer los datos
+    const {nombre, email, password} = req.body;
+
+    // verificar que el usuarios no este duplicado
+    const existeUsuario = await Usuario.findOne({ where : {email}});
+    if(existeUsuario){
+        return res.render('auth/registro', {
+            pagina : 'Crear Cuenta',
+            errores : [{msg : 'El usaurio ya esta registrado'}],
+            usuario :{ 
+                nombre : req.body.nombre,
+                email : req.body.email
+            }
+        })
+    }
+    
+
     const usuario = await Usuario.create(req.body);
     res.json(usuario)   
 }
